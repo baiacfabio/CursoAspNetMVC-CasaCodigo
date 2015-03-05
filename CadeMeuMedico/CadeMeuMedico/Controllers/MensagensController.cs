8f2fcb0cf3506de4b3c72b369a97b703addc5f96
Fabio Cezar Baía <baiacfabio@gmail.com>
2015-03-05 18:36:46 -0300

    Criando MensagensController

---
+++ CadeMeuMedico/CadeMeuMedico/Controllers/MensagensController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadeMeuMedico.Controllers
+{
+    public class MensagensController : Controller
+    {
+        //
+        // GET: /Mensagens/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
