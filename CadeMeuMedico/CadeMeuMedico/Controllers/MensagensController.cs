23e6778f694a7635d086182d18ac41ce8338db49
Fabio Cezar Baía <baiacfabio@gmail.com>
2015-03-05 18:40:28 -0300

    Removendo action Index e adicionando duas novas actions

--- CadeMeuMedico/CadeMeuMedico/Controllers/MensagensController.cs
+++ CadeMeuMedico/CadeMeuMedico/Controllers/MensagensController.cs
@@ -8,12 +8,14 @@ namespace CadeMeuMedico.Controllers
 {
     public class MensagensController : Controller
     {
-        //
-        // GET: /Mensagens/
-
-        public ActionResult Index()
+        public ActionResult BomDia()
         {
-            return View();
+            return Content("Bom dia... hoje você acordou cedo!");
+        }
+
+        public ActionResult BoaTarde()
+        {
+            return Content("Boa tarde... não durma na mesa do trabalho!");
         }
 
     }
