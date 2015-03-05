e37b6b9be59014be8a3f5ce7935cac8b14edc06b
Fabio Cezar Baía <baiacfabio@gmail.com>
2015-03-05 10:12:34 -0300

    Criando models

---
+++ AplicacaoComCodeFirst/AplicacaoComCodeFirst/Models/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace AplicacaoComCodeFirst.Models
+{
+    public class BlogContext : DbContext
+    {
+        
+        //public BlogContext() : base("name=BlogContext")
+        /*public BlogContext() : base("name=DefaultConnection")
+            
+        {
+            //Database.Connection.ConnectionString = @"Data Source=mga-baia\localdb";
+
+            //Database.Connection.ConnectionString = @"data source=FABRCIOSANC36FC\SQLEXPRESS;
+//initial catalog=BlogBDLivro; Integrated Security=SSPI";
+        }*/
+         
+        public DbSet<Posts> Posts { get; set; }
+
+        public DbSet<Categorias> Categorias { get; set; }
+    }
+}
