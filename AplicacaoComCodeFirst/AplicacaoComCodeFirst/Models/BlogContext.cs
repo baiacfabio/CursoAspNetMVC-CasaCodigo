450d14c6bbc526abaaae01b60f1bb5e8e82e1a5f
Fabio Cezar Baía <baiacfabio@gmail.com>
2015-03-05 10:17:30 -0300

    Removendo código nao utilizado

--- AplicacaoComCodeFirst/AplicacaoComCodeFirst/Models/BlogContext.cs
+++ AplicacaoComCodeFirst/AplicacaoComCodeFirst/Models/BlogContext.cs
@@ -5,16 +5,6 @@ namespace AplicacaoComCodeFirst.Models
     public class BlogContext : DbContext
     {
         
-        //public BlogContext() : base("name=BlogContext")
-        /*public BlogContext() : base("name=DefaultConnection")
-            
-        {
-            //Database.Connection.ConnectionString = @"Data Source=mga-baia\localdb";
-
-            //Database.Connection.ConnectionString = @"data source=FABRCIOSANC36FC\SQLEXPRESS;
-//initial catalog=BlogBDLivro; Integrated Security=SSPI";
-        }*/
-         
         public DbSet<Posts> Posts { get; set; }
 
         public DbSet<Categorias> Categorias { get; set; }
