2ab8b87b744e603d91a530589cebf253add44b15
Fabio Baía <fabio-baia@benner.com.br>
2015-03-05 19:06:14 -0300

    Implementando MedicosController

---
+++ CadeMeuMedico/CadeMeuMedico/Controllers/MedicosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CadeMeuMedico.Models;
+
+namespace CadeMeuMedico.Controllers
+{
+    public class MedicosController : Controller
+    {
+        
+        private EntidadesCadeMeuMedicoBD db = new EntidadesCadeMeuMedicoBD();
+
+        public ActionResult Index()
+        {
+            var medicos = db.Medicos.Include("Cidades").Include("Especialidades").ToList();
+            
+            return View(medicos);
+        }
+
+    }
+}
